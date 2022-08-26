a31674a2a3e6e8b7227c85ad50b52eaf38fdb94a
vrettasm <vrettasm@gmail.com>
2022-08-26 11:48:52 +0200

    Update the Lorenz63 to reflect the changes in the SP.

--- src/dynamical_systems/lorenz_63.py
+++ src/dynamical_systems/lorenz_63.py
@@ -1,19 +1,20 @@
 import numpy as np
 from numba import njit
+from numpy import array as array_t
 from scipy.linalg import cholesky, LinAlgError
 from src.dynamical_systems.stochastic_process import StochasticProcess
 
 
 @njit
-def _l63(state, u):
+def _l63(state: array_t, theta: array_t) -> array_t:
     """
-    Lorenz63 function.
+    Lorenz63 auxiliary function.
 
     https://en.wikipedia.org/wiki/Lorenz_system
 
     :param state: (x, y, z).
 
-    :param u: model parameters (sigma=10, rho=28, beta=8/3).
+    :param theta: model parameters (sigma=10, rho=28, beta=8/3).
 
     :return: One step ahead in the equation.
     """
@@ -22,88 +23,89 @@ def _l63(state, u):
     x, y, z = state
 
     # Unpack parameters.
-    sigma, rho, beta = u
+    sigma, rho, beta = theta
 
     # Lorenz equations.
-    dx = np.array([sigma * (y - x),
-                   (rho - z) * x - y,
-                   x * y - beta * z])
-    # Return dx.
-    return dx
+    d_state = array_t([sigma * (y - x),
+                       (rho - z) * x - y,
+                       x * y - beta * z])
+    # Return d_state/dt.
+    return d_state
 # _end_def_
 
 
 class Lorenz63(StochasticProcess):
     """
-    Class that model the Lorenz 3D (1963) dynamical system.
+    Information about the Lorenz 3D (1963) dynamical system:
 
     https://en.wikipedia.org/wiki/Lorenz_system
     """
 
-    __slots__ = ("sigma_", "theta_", "sigma_inv")
+    __slots__ = ("_sigma", "_theta", "_sigma_inverse")
 
-    def __init__(self, sigma, theta, r_seed=None):
+    def __init__(self, sigma: array_t, theta: array_t, r_seed: int = None):
         """
         Default constructor of the L63 object.
 
-        :param sigma: noise diffusion coefficient.
+        :param sigma: (numpy array) noise diffusion coefficient.
 
-        :param theta: drift model vector.
+        :param theta: (numpy array) drift model vector.
 
-        :param r_seed: random seed.
+        :param r_seed: (int) random seed.
         """
+
         # Call the constructor of the parent class.
-        super().__init__(r_seed, n_dim=3)
+        super().__init__(r_seed=r_seed)
 
         # Display class info.
         print(" Creating Lorenz-63 process.")
 
-        # Make sure the inputs are arrays.
+        # Make sure the inputs are numpy arrays.
         sigma = np.asarray(sigma, dtype=float)
         theta = np.asarray(theta, dtype=float)
 
         # Check the dimensions of the input.
         if sigma.ndim == 0:
             # Diagonal matrix (from scalar).
-            self.sigma_ = sigma * np.eye(3)
+            self._sigma = sigma * np.eye(3)
 
         elif sigma.ndim == 1:
             # Diagonal matrix (from vector).
-            self.sigma_ = np.diag(sigma)
+            self._sigma = np.diag(sigma)
 
         elif sigma.ndim == 2:
             # Full Matrix.
-            self.sigma_ = sigma
+            self._sigma = sigma
 
         else:
             raise ValueError(f" {self.__class__.__name__}:"
                              f" Wrong input dimensions: {sigma.ndim}")
         # _end_if_
 
-        # Check the dimensionality.
-        if self.sigma_.shape != (3, 3):
+        # Check for the correct matrix dimensions.
+        if self._sigma.shape != (3, 3):
             raise ValueError(f" {self.__class__.__name__}:"
-                             f" Wrong matrix dimensions: {self.sigma_.shape}")
+                             f" Wrong matrix dimensions: {self._sigma.shape}")
         # _end_if_
 
         # Check for positive definiteness.
-        if np.all(np.linalg.eigvals(self.sigma_) > 0.0):
+        if np.all(np.linalg.eigvals(self._sigma) > 0.0):
 
             # Invert the sigma matrix.
-            self.sigma_inv = np.linalg.inv(self.sigma_)
+            self._sigma_inverse = np.linalg.inv(self._sigma)
         else:
             raise RuntimeError(f" {self.__class__.__name__}: Noise matrix"
-                               f" {self.sigma_} is not positive definite.")
+                               f" {self._sigma} is not positive definite.")
         # _end_if_
 
-        # Check the size of the vector.
+        # Check the size of the drift vector.
         if theta.size == 3:
 
             # Store the drift vector.
-            self.theta_ = theta
+            self._theta = theta
         else:
             raise RuntimeError(f" {self.__class__.__name__}: Drift vector"
-                               f" {self.theta_} is not [3 x 1].")
+                               f" {self._theta} is not [3 x 1].")
         # _end_if_
 
     # _end_def_
@@ -111,46 +113,47 @@ class Lorenz63(StochasticProcess):
     @property
     def theta(self):
         """
-        Accessor method.
+        Accessor method (getter).
 
         :return: the drift parameter.
         """
-        return self.theta_
+        return self._theta
 
     # _end_def_
 
     @theta.setter
-    def theta(self, new_value):
+    def theta(self, new_value: array_t):
         """
-        Accessor method.
+        Accessor method (setter).
 
         :param new_value: for the drift parameter.
 
         :return: None.
         """
-        self.theta_ = np.asarray(new_value, dtype=float)
+        self._theta = np.asarray(new_value, dtype=float)
     # _end_def_
 
     @property
     def sigma(self):
         """
-        Accessor method.
+        Accessor method (getter).
 
         :return: the system noise parameter.
         """
-        return self.sigma_
+        return self._sigma
 
     # _end_def_
 
     @sigma.setter
-    def sigma(self, new_value):
+    def sigma(self, new_value: array_t):
         """
-        Accessor method.
+        Accessor method (setter).
 
         :param new_value: for the sigma diffusion.
 
         :return: None.
         """
+
         # Make sure the input is array.
         new_value = np.asarray(new_value, dtype=float)
 
@@ -163,10 +166,10 @@ class Lorenz63(StochasticProcess):
         # Check for positive definiteness.
         if np.all(np.linalg.eigvals(new_value) > 0.0):
             # Make the change.
-            self.sigma_ = new_value
+            self._sigma = new_value
 
             # Update the inverse matrix.
-            self.sigma_inv = np.linalg.inv(self.sigma_)
+            self._sigma_inverse = np.linalg.inv(self._sigma)
         else:
             raise RuntimeError(f" {self.__class__.__name__}: Noise matrix"
                                f" {new_value} is not positive definite.")
@@ -177,69 +180,73 @@ class Lorenz63(StochasticProcess):
     @property
     def inverse_sigma(self):
         """
-        Accessor method.
+        Accessor method (getter).
 
-        :return: the inverse of diffusion noise parameter.
+        :return: the inverse of diffusion noise matrix.
         """
-        return self.sigma_inv
+        return self._sigma_inverse
 
     # _end_def_
 
-    def make_trajectory(self, t0, tf, dt=0.01):
+    def make_trajectory(self, t0: float, tf: float, dt: float = 0.01):
         """
         Generates a realizations of the Lorenz63 (3D)
         dynamical system, within a specified time-window.
 
-        :param t0: initial time point.
+        :param t0: (float) initial time point.
 
-        :param tf: final time point.
+        :param tf: (float) final time point.
 
-        :param dt: discrete time-step.
+        :param dt: (float) discrete time-step.
 
         :return: None.
         """
 
         # Create locally a time-window.
-        tk = np.arange(t0, tf + dt, dt)
+        tk = np.arange(t0, tf + dt, dt, dtype=float)
 
         # Number of actual trajectory samples.
         dim_t = tk.size
 
-        # Default starting point.
+        # Default starting point [1.0, 1.0, 1.0].
         x0 = np.ones(3)
 
         # Initial conditions time step.
-        delta_t = 1.0e-3
+        delta_t = 1.0E-3
 
-        # BURN IN: This is used to get in the attractor.
+        # BURN IN: This is used to get the state vector inside
+        # the "butterfly" attractor.
         for t in range(5000):
-            x0 = x0 + _l63(x0, self.theta_) * delta_t
+            x0 = x0 + _l63(x0, self._theta) * delta_t
         # _end_for_
 
         # Allocate the array.
         x = np.zeros((dim_t, 3))
 
-        # Start with the new point.
+        # Start the sample path with the new point.
         x[0] = x0
 
-        # Compute the Cholesky decomposition of input matrix.
+        # Compute the Cholesky decomposition of noise matrix.
         try:
-            ek = cholesky(self.sigma_ * dt)
+            # Notice the scaling with 'dt'.
+            ek = cholesky(self._sigma * dt)
+
         except LinAlgError:
+
             # Show a warning message.
             print(" Warning : Input matrix was not positive definite."
                   " The diagonal elements will be used instead.")
 
-            # If it fails use the diagonal only.
-            ek = np.sqrt(np.eye(3) * self.sigma_ * dt)
+            # If it fails use only the diagonal elements.
+            ek = np.sqrt(np.eye(3) * self._sigma * dt)
         # _end_try_
 
         # Random variables.
         ek = ek.dot(self.rng.standard_normal((3, dim_t))).T
 
-        # Create the path by solving the "stochastic" Diff.Eq. iteratively.
+        # Create the path by solving the SDE iteratively.
         for t in range(1, dim_t):
-            x[t] = x[t-1] + _l63(x[t-1], self.theta_) * dt + ek[t]
+            x[t] = x[t-1] + _l63(x[t-1], self._theta) * dt + ek[t]
         # _end_for_
 
         # Store the sample path (trajectory).
