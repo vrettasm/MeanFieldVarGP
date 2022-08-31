Fix the gradients of the cost function.

src/var_bayes/free_energy.py
@@ -6,7 +6,8 @@ from src.numerical.utilities import cholesky_inv, log_det
 class FreeEnergy(object):
 
     def __init__(self, tk: array_t, mu0: array_t, tau0: array_t,
-                 drift_func: callable, sde_theta: array_t, sde_sigma: array_t,
+                 drift_func: callable, grad_mean: callable, grad_vars: callable,
+                 sde_theta: array_t, sde_sigma: array_t,
                  obs_times: array_t, obs_values: array_t, obs_noise: array_t,
                  h_operator: array_t = None):
         """
@@ -19,6 +20,10 @@ class FreeEnergy(object):
         :param tau0: (prior) variance value(s) at time t=0.
 
         :param drift_func: (callable) function of the system.
+
+        :param grad_mean: (callable) function of the system gradients.
+
+        :param grad_vars: (callable) function of the system gradients.
 
         :param sde_theta: SDE drift model parameters.
 
@@ -36,6 +41,12 @@ class FreeEnergy(object):
         # SDE drift function.
         self.drift_fun = drift_func
 
+        # Gradients of the SDE with respect to the mean points.
+        self.grad_mp = grad_mean
+
+        # Gradients of the SDE with respect to the variance points.
+        self.grad_sp = grad_vars
+
         # Prior mean (t=0).
         self._mu0 = np.asarray(mu0, dtype=float)
 
@@ -52,11 +63,12 @@ class FreeEnergy(object):
 
         # Sanity check.
         if not np.all(np.diff(self.tk) > 0.0):
-            raise ValueError(f" {self.__class__.__name__}: Time window [t0, tf] is not increasing.")
+            raise ValueError(f" {self.__class__.__name__}:"
+                             f" Time window [t0, tf] is not increasing.")
         # _end_if_
 
-        # Make sure the observation parameters
-        # are entered correctly as arrays.
+        # Make sure the observation related parameters
+        # are entered correctly as numpy arrays.
         self.obs_times = np.asarray(obs_times, dtype=float)
         self.obs_noise = np.asarray(obs_noise, dtype=float)
         self.obs_values = np.asarray(obs_values, dtype=float)
@@ -95,7 +107,8 @@ class FreeEnergy(object):
         # Sanity check.
         if self.dim_d > self.dim_D:
             raise RuntimeError(f" {self.__class__.__name__}: System dimensions."
-                               f" {self.dim_d} should be less than, or equal, to {self.dim_D}.")
+                               f" {self.dim_d} should be less than, or equal,"
+                               f" to {self.dim_D}.")
         # _end_if_
 
         # Sanity check.
@@ -105,21 +118,13 @@ class FreeEnergy(object):
                                f" to observation values {self.obs_values.shape[0]}.")
         # _end_if_
 
-        # Number of mean points (for a 3rd order polynomial).
-        mean_points = (3 * self.num_M + 4)
+        # Total number of mean points (for a 3rd order polynomial)
+        # scaled with the dimensions.
+        self.num_mp = self.dim_D * (3 * self.num_M + 4)
 
-        # Total number of mean points (scaled with the dimensions).
-        self.total_num_mp = self.dim_D * mean_points
-
-        # Number of variance points (for a 2nd order polynomial).
-        vars_points = (2 * self.num_M + 3)
-
-        # Number of variance points (scaled with the dimensions).
-        # self.total_num_sp = self.dim_D * vars_points
-
-        # Indexes of the observations at the mean/var points.
-        self.Tkm = np.arange(3, mean_points - 1, step=3)
-        self.Tks = np.arange(2, vars_points - 1, step=2)
+        # Indexes of the observations at the mean/variance points.
+        self.ikm = [3*i for i in range(1, self.num_M+1)]
+        self.iks = [2*i for i in range(1, self.num_M+1)]
 
     # _end_def_
 
@@ -139,6 +144,8 @@ class FreeEnergy(object):
         Accessor method (setter).
 
         NOTE: This should be used only if we optimize the prior moments.
+
+        :param new_value: the new value we want to set.
 
         :return: None.
         """
@@ -161,6 +168,8 @@ class FreeEnergy(object):
         Accessor method (setter).
 
         NOTE: This should be used only if we optimize the prior moments.
+
+        :param new_value: the new value we want to set.
 
         :return: None.
         """
@@ -185,6 +194,8 @@ class FreeEnergy(object):
 
         NOTE: This should be used only if we optimize the drift parameters.
 
+        :param new_value: the new value we want to set.
+
         :return: None.
         """
         self.theta = np.asarray(new_value, dtype=float)
@@ -207,6 +218,8 @@ class FreeEnergy(object):
 
         NOTE: This should be used only if we optimize the diffusion parameters.
 
+        :param new_value: the new value we want to set.
+
         :return: None.
         """
         self.sigma = np.asarray(new_value, dtype=float)
@@ -224,16 +237,15 @@ class FreeEnergy(object):
         :param s0: marginal variance at t=0, (dim_D).
 
         :return: energy of the initial state E0, (scalar)
-        and it derivative with respect ot 'm0' and 's0'.
+        and its derivatives with respect ot 'm0' and 's0'.
         """
 
         # Difference of the two "mean" vectors.
         z0 = m0 - self._mu0
 
         # Energy of the initial moment.
-        # NOTE: This formula works only because the matrices 'tau0' and 's0' are
-        # diagonal, hence we consider only their diagonal elements, and we work
-        # only with vectors.
+        # NOTE: This formula works only because the matrices 'tau0'
+        # and 's0' are diagonal, and we work only with vectors.
         E0 = 0.5 * (np.log(np.prod(self._tau0 / s0)) +
                     np.sum((z0**2 + s0 - self._tau0) / self._tau0))
 
@@ -242,15 +254,26 @@ class FreeEnergy(object):
 
         # Compute the gradients. We use the "atleast_1d" to ensure
         # that the scalar cases (even though rare) will be covered.
-        dE_dm0 = np.atleast_1d(z0 / self._tau0)
-        dE_ds0 = np.atleast_1d(0.5 * (one_ / self._tau0 - one_ / s0))
+        dE0_dm0 = np.atleast_1d(z0 / self._tau0)
+        dE0_ds0 = np.atleast_1d(0.5 * (one_ / self._tau0 - one_ / s0))
 
-        # Kullback-Liebler value and its derivative, at t=0.
-        return E0, np.concatenate((dE_dm0, dE_ds0), axis=0)
+        # Kullback-Liebler and its derivatives at time t=0,
+        # (i.e. dKL0/dm(0) and dKL0/ds(0))
+        return E0, dE0_dm0, dE0_ds0
 
     # _end_def_
 
     def E_sde(self, mean_pts, vars_pts):
+        """
+        Energy from SDE prior process.
+
+        :param mean_pts: optimized mean points.
+
+        :param vars_pts: optimized variance points.
+
+        :return: Energy from the SDE prior process (scalar) and
+        its gradients with respect to the mean and variance points.
+        """
 
         # Number of discrete intervals
         # (between the observations).
@@ -259,14 +282,21 @@ class FreeEnergy(object):
         # Initialize energy for the SDE.
         Esde = 0.0
 
+        # Initialize gradients arrays.
+        # > dEsde_dm := dEsde(tk)/dm(tk)
+        # > dEsde_ds := dEsde(tk)/ds(tk)
+        dEsde_dm = np.zeros(L, self.dim_D, 4)
+        dEsde_ds = np.zeros(L, self.dim_D, 3)
+
         # Calculate energy from all 'L' time intervals.
         for n in range(L):
 
             # Take the limits of the observation's interval.
             ti, tj = self.obs_times[n], self.obs_times[n+1]
 
-            # Distance between the two observations.
-            # NOTE: This should not change for equally spaced observations.
+            # NOTE: This should not change for equally spaced
+            # observations. This is here to ensure that small
+            # 'dt' deviations will not affect the algorithm.
             delta_t = np.abs(tj-ti)
 
             # Mid-point intervals (for the evaluation of the Esde function).
@@ -281,35 +311,75 @@ class FreeEnergy(object):
             # Get the SDE (partial) energy for the n-th interval.
             Esde += self.drift_fun(self.theta, self.sigma, h, c,
                                    nth_mean_points, nth_vars_points)
+
+            # Compute the partial gradients of the mean points.
+            dEsde_dm[n] = 0.5*self.grad_mp(self.theta, self.sigma, h, c,
+                                           nth_mean_points, nth_vars_points)
+
+            # Compute the partial gradients of the variance points.
+            dEsde_ds[n] = 0.5*self.grad_sp(self.theta, self.sigma, h, c,
+                                           nth_mean_points, nth_vars_points)
         # _end_for_
 
         # Return the total energy (including the correct scaling).
-        # NOTE: This is Eq. (27) in the paper.
-        return 0.5 * Esde
+        # and its gradients with respect ot the mean and variance
+        # (optimized) points.
+        return 0.5 * Esde, dEsde_dm, dEsde_ds
     # _end_def_
 
-    def E_obs(self, mean_pts, var_pts):
+    def E_obs(self, mean_pts, vars_pts):
+        """
+        Energy from Gaussian likelihood.
+
+        :param mean_pts: optimized mean points.
+
+        :param vars_pts: optimized variance points.
+
+        :return: Energy from the observation likelihood (scalar)
+        and its gradients with respect to the mean and variance
+        points.
+        """
 
         # Inverted Ri and Cholesky factor Qi.
         Ri, Qi = cholesky_inv(self.obs_noise)
 
-        # Auxiliary quantity no.1.
+        # Auxiliary quantity (for the E_obs) no.1.
         Z = Qi.dot(self.obs_values - self.h_operator.dot(mean_pts))
 
-        # Auxiliary quantity no.2.
-        W = Ri.diagonal().T.dot(self.h_operator.dot(var_pts))
+        # Auxiliary quantity (for the E_obs) no.2.
+        W = Ri.diagonal().T.dot(self.h_operator.dot(vars_pts))
+
+        # These are the derivatives of E_{obs} w.r.t. the mean/var points.
+        kappa_1 = -self.h_operator.dot(Ri).dot(self.obs_values -
+                                               self.h_operator.dot(mean_pts))
+
+        # Note that the dEobs(k)/ds(k) is identical for all observations.
+        kappa_2 = 0.5 * np.diag(self.h_operator.T.dot(Ri).dot(self.h_operator))
 
         # Initialize observations' energy.
         Eobs = 0.0
 
-        # Calculate from all 'M' observations.
+        # Initialize gradients arrays.
+        dEobs_dm = np.zeros(self.num_M, self.dim_d)
+        dEobs_ds = np.zeros(self.num_M, self.dim_d)
+
+        # Calculate partial energies from all 'M' observations.
+        # NOTE: The gradients are given by:
+        #   1. dEobs(k)/dm(k) := -H'*Ri*(yk-h(xk))
+        #   2. dEobs(k)/ds(k) := 0.5*diag(H'*Ri*H)
         for k in range(self.num_M):
 
-            # Get the auxiliary value at this point.
+            # Get the auxiliary value.
             Zk = Z[k]
 
             # Compute the energy of the k-th observation.
             Eobs += Zk.T.dot(Zk) + W[k]
+
+            # Gradient of E_{obs} w.r.t. m(tk).
+            dEobs_dm[k] = kappa_1[k]
+
+            # Gradient of E_{obs} w.r.t. S(tk).
+            dEobs_ds[k] = kappa_2
         # _end_for_
 
         # Logarithm of 2*pi.
@@ -319,28 +389,48 @@ class FreeEnergy(object):
         Eobs += self.num_M * (self.dim_d * log2pi + log_det(self.obs_noise))
 
         # Return the total observation energy and its gradients.
-        # NOTE: This is Eq. (17) in the paper.
-        return 0.5 * Eobs, ...
+        return 0.5 * Eobs, dEobs_dm, dEobs_ds
     # _end_def_
 
     def E_total(self, x):
+        """
+        TBD
+
+        :param x: tbd...
+
+        :return: tbd...
+        """
 
         # Separate the mean from the variance points.
-        mean_points = np.reshape(x[0:self.total_num_mp],
+        mean_points = np.reshape(x[0:self.num_mp],
                                  (self.dim_D, (3*self.num_M + 4)))
 
         # The variance points are in log-space to ensure positivity,
         # so we pass them through the exponential function first.
-        vars_points = np.reshape(np.exp(x[self.total_num_mp:]),
+        vars_points = np.reshape(np.exp(x[self.num_mp:]),
                                  (self.dim_D, (2*self.num_M + 3)))
+
+        # Energy (and gradients) from the initial moment (t=0).
+        E0, dE0_dm0, dE0_ds0 = self.E_kl0(mean_points[:, 0],
+                                          vars_points[:, 0])
+
+        # Energy from the SDE (and gradients).
+        Esde, dEsde_dm, dEsde_ds = self.E_sde(mean_points,
+                                              vars_points)
+
+        # Energy from the observations' likelihood (and gradients).
+        Eobs, dEobs_dm, dEobs_ds = self.E_obs(mean_points[:, self.ikm],
+                                              vars_points[:, self.iks])
+
+        # Put all the energy values together.
+        E_tot = E0 + Esde + Eobs
 
         # Return the total (free) energy as the sum of the individual
         # components. NOTE: If we want to optimize the hyperparameter
         # we should add another term, e.g. E_param, and include it in
         # the total sum of energy values.
-        return self.E_kl0(mean_points[:, 0], vars_points[:, 0]) +\
-               self.E_sde(mean_points, vars_points) +\
-               self.E_obs(mean_points[:, self.Tkm], vars_points[:, self.Tks])
+        return E_tot, grad_tot
+
     # _end_def_
 
 # _end_class_