5c99144c417f1809a255d9f99547c10c75b9ce67
vrettasm <vrettasm@gmail.com>
2022-09-14 13:03:16 +0200

    Changed the names of the energy and gradient functions, from L3D to L63.

--- src/dynamical_systems/lorenz_63.py
+++ src/dynamical_systems/lorenz_63.py
@@ -193,7 +193,7 @@ class Lorenz63(StochasticProcess):
         for i in range(3):
 
             # Load the energy file.
-            with open(Path(current_dir / f"energy_functions/L3D_Esde_{i}.sym"), "rb") as sym_Eqn:
+            with open(Path(current_dir / f"energy_functions/L63_Esde_{i}.sym"), "rb") as sym_Eqn:
 
                 # Append the energy function.
                 self.Esde.append(njit(dl_load(sym_Eqn)))
@@ -204,7 +204,7 @@ class Lorenz63(StochasticProcess):
             # _end_with_
 
             # Load the mean-gradient file.
-            with open(Path(current_dir / f"gradient_functions/dL3D_Esde_dM{i}.sym"), "rb") as sym_Eqn:
+            with open(Path(current_dir / f"gradient_functions/dL63_Esde_dM{i}.sym"), "rb") as sym_Eqn:
 
                 # Append the grad_DM function.
                 self.dEsde_dm.append(njit(dl_load(sym_Eqn)))
@@ -215,7 +215,7 @@ class Lorenz63(StochasticProcess):
             # _end_with_
 
             # Load the variance-gradient file.
-            with open(Path(current_dir / f"gradient_functions/dL3D_Esde_dS{i}.sym"), "rb") as sym_Eqn:
+            with open(Path(current_dir / f"gradient_functions/dL63_Esde_dS{i}.sym"), "rb") as sym_Eqn:
 
                 # Append the grad_DS function.
                 self.dEsde_ds.append(njit(dl_load(sym_Eqn)))
